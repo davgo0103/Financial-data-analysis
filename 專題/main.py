ae13a36de88303fd2b9118326b9ccbb318da4035
davgo0103 <davvgo@gmail.com>
2022-12-14 15:06:26 +0800

    Update

--- 專題/main.py
+++ 專題/main.py
@@ -13,9 +13,9 @@ import os
 ################################## Config ##################################
 line_token = '42CbyNEG6cR7rCAIVnZao0TxTNNsp1Ad54PWAmVBReO' #TOKEN   
 dc_url = 'https://discord.com/api/webhooks/782196400003219456/l-sbfRV08zxjFpOAjC_WwfHxfc_-dNgsXGOwmsPBwWaU_EKVZOLcUfyiMPmv-Pp4R1VD' #Discord WebHook URL
-SEND_TYPE = 'ALL' #訊息傳遞種類，可填 LINE、Discord、ALL 注意大小寫
+SEND_TYPE = 'LINE' #訊息傳遞種類，可填 LINE、Discord、ALL 注意大小寫
 DATE = "" #留空使用預設日期 格式:yyyymmdd
-OPEN_SEND = "TRUE" #開啟程式時是否先執行一次? 填入 TRUE 每次執行時都會先跑一次
+OPEN_SEND = "FALSE" #開啟程式時是否先執行一次? 填入 TRUE 每次執行時都會先跑一次
 ############################################################################
 
 def leave():
